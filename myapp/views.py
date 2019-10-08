6de51203ad9d90cdb35c74de6dbec1d26f18f14a
hyeji0503 <hyeji9605@gmail.com>
2019-10-09 02:55:01 +0900

    heroku1

--- myapp/views.py
+++ myapp/views.py
@@ -6,14 +6,12 @@ from gtts import gTTS
 # Create your views here.
 
 def home(request):
-
     tts_content = request.GET.get('tts_content', '')
     if tts_content:
         gtts2 = gTTS(text=tts_content, lang = 'ko')
         gtts2.save("%s.mp3" % os.path.join('./TTS2/', "gtts2"))
         print("%s.mp3" % os.path.join('./TTS2/', "gtts2"))
-        return redirect('home')
-        
+        return redirect('home')        
     return render(request, 'home.html')
 
 def test(request):
