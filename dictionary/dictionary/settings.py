e2e2ae743ec650b91dc89ce5a6349f24ffdc501e
hyeji0503 <hyeji9605@gmail.com>
2019-10-08 16:43:52 +0900

    노가다 and gtts

--- dictionary/dictionary/settings.py
+++ dictionary/dictionary/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp', 'static'),
-    os.path.join('TTS'),
+    os.path.join('TTS2'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
